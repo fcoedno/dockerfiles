5a2fb7529f6ed1b85b8f9ffadc67c601d446f213
Francisco Edno <francisco.edno@s2it.com.br>
2018-09-16 17:47:30 -0300

    Fixed nginx-dev

--- nginx/1.13.8-dev/entrypoint.sh
+++ nginx/1.13.8-dev/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-envsubst '$NGINX_HOST $NGINX_PORT $NGINX_ROOT' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_HOST $NGINX_PORT $NGINX_ROOT $NGINX_FPM_HOST $NGINX_FPM_PORT' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
